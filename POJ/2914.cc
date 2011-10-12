8dfea34aba473768cbab2d4cc84325505f0ee395
Leewings Ac <osideal@gmail.com>
2011-10-12 22:29:43 +0800

    [ACM] daily

---
+++ POJ/2914.cc
@@ -0,0 +1,89 @@
+/*
+ *  SRC: POJ 2914
+ * PROB: Minimum Cut
+ * ALGO: Stoer-Wagner
+ * DATE: Oct 12, 2011 
+ * COMP: g++
+ *
+ * Created by Leewings Ac
+ */
+
+#include <cstdio>
+#include <cstring>
+
+const int inf = 0x3f3f3f3f;
+const int MAX_N = 555;
+
+int n, m;
+int map[MAX_N][MAX_N];
+
+bool A[MAX_N];
+int v[MAX_N], w[MAX_N];
+
+int stoer_wagner(int n)
+{
+    int res = inf;
+    for (int i = 0; i < n; i++) v[i] = i;
+
+    while (n > 1) {
+        A[v[0]] = true;
+
+        for (int i = 1; i < n; i++) {
+            A[v[i]] = false;
+            w[v[i]] = map[v[0]][v[i]];
+        }
+
+        for (int prev = 0, i = 1; i < n; i++) {
+            int curr = -1;
+            for (int j = 1; j < n; j++)
+                if (!A[v[j]] && (curr == -1 || w[v[j]] > w[v[curr]]))
+                    curr = j;
+            A[v[curr]] = true;
+
+            if (i == n - 1) {
+                if (res > w[v[curr]]) res = w[v[curr]];
+                for (int j = 0; j < n; j++)
+                    map[v[j]][v[prev]] = map[v[prev]][v[j]] += map[v[curr]][v[j]];
+                v[curr] = v[--n];
+                break;
+            }
+
+            prev = curr;
+            for (int j = 1; j < n; j++)
+                if (!A[v[j]]) w[v[j]] += map[v[curr]][v[j]];
+        }
+    }
+
+    return res;
+}
+
+bool con_vis[MAX_N];
+int connected(int u)
+{
+    con_vis[u] = true;
+
+    int res = 1;
+    for (int i = 0; i < n; i++)
+        if (map[u][i] && !con_vis[i])
+            res += connected(i);
+    return res;
+}
+
+int main()
+{
+    while (scanf("%d%d", &n, &m) != EOF) {
+        memset(map, 0, sizeof(map));
+        memset(con_vis, 0, sizeof(con_vis));
+
+        for (int i = 0; i < m; i++) {
+            int a, b, c;
+            scanf("%d%d%d", &a, &b, &c);
+            map[a][b] = map[b][a] += c;
+        }
+        
+        if (connected(0) != n) puts("0");
+        else printf("%d\n", stoer_wagner(n));
+    }
+
+    return 0;
+}
