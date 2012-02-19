0af26eb927d71812a8778807bcb05e537ce7acbb
Leewings Ac <osideal@gmail.com>
2012-02-19 10:41:27 +0800

    [Codeforces] Add java template

---
+++ Codeforces/Main.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException
+    {
+        new Prob().solve();
+    }
+}
+
+class Prob {
+    void solve() throws IOException
+    {
+        MyReader in = new MyReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        // ...
+
+        out.flush();
+    }
+
+    void debug(Object...x)
+    {
+        System.out.println(Arrays.deepToString(x));
+    }
+}
+
+class MyReader {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer in;
+
+    boolean hasNext() throws IOException
+    {
+        if (in == null || in.hasMoreTokens()) return true;
+        String line = br.readLine();
+        if (line == null) return false;
+        in = new StringTokenizer(line);
+        return true;
+    }
+
+    String next() throws IOException
+    {
+        while (in == null || !in.hasMoreTokens())
+            in = new StringTokenizer(br.readLine());
+        return in.nextToken();
+    }
+
+    int nextInt() throws IOException { return Integer.parseInt(next()); }
+    long nextLong() throws IOException { return Long.parseLong(next()); }
+    double nextDouble() throws IOException { return Double.parseDouble(next()); }
+    BigInteger nextBigInteger() throws IOException { return new BigInteger(next()); }
+    BigDecimal nextBigDecimal() throws IOException { return new BigDecimal(next()); }
+}
