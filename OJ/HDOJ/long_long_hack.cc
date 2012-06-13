32057eee7ce472be76cf6d776ffbe37bab0c5831
Leewings Ac <osideal@gmail.com>
2012-06-14 01:54:39 +0800

    [ACM] HDOJ long_long_hack.cc

---
+++ OJ/HDOJ/long_long_hack.cc
@@ -0,0 +1,22 @@
+// method 0
+// usage: printf(LLD" "LLD"\n", a, b);
+#ifdef ONLINE_JUDGE
+#define LLD "%I64d"
+#else
+#define LLD "%lld"
+#endif
+
+// method 1
+// usage: myprintf("%lld %lld\n", a, b);
+inline void myprintf(string s, ...)
+{
+    va_list ap;
+    va_start(ap, s);
+#ifdef ONLINE_JUDGE
+    int pos;
+    while ((pos = s.find("lld")) != string::npos) {
+        s.replace(pos, 3, "I64d");
+    }
+#endif
+    vprintf(s.c_str(), ap);
+}
