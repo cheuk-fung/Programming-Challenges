238669cc71689611cbd6cc747e3331fed47a0180
Leewings Ac <osideal@gmail.com>
2012-07-28 00:11:26 +0800

    [ACM] hello, sgu

---
+++ OJ/SGU/100.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args)
+    {
+        Scanner cin = new Scanner(System.in);
+        System.out.println(cin.nextInt() + cin.nextInt());
+    }
+}
