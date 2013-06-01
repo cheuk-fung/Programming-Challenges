f43cf1f7019e28871c45da9119aa102fd2c28aca
Cheuk-fung Ng <osideal@gmail.com>
2013-06-01 19:45:58 +0800

    [ACM] Improve Suffix Array

--- Templates/suffix-array.cc
+++ Templates/suffix-array.cc
@@ -17,85 +17,80 @@ using std::swap;
 class Suffix_Array {
     // use Doubling Algorithm
 
-    private:
+    public:
         const static int MAXLEN = 200012;
         const static int MAXCHAR = 128;
 
-        int *nrank;
-        int rbuf[2][MAXLEN]; // used for ranking
+        int n;			// length of s
+        char s[MAXLEN];
 
-        int cnt[MAXLEN]; // used for counting sort
-
-        void csort(int k, int range)
-        {
-            int t = 0;
-            for (int i = len - k; i < len; i++) nrank[t++] = i;
-            for (int i = 0; i < len; i++)
-                if (suff[i] >= k) nrank[t++] = suff[i] - k;
-
-            for (int i = 0; i < len; i++) cnt[rank[i]]++;
-            for (int i = 1; i < range; i++) cnt[i] += cnt[i - 1];
-            for (int i = len - 1, j; i >= 0; i--) {
-                j = nrank[i];
-                suff[--cnt[rank[j]]] = j;
-            }
-
-            memset(cnt, 0, sizeof(int) * range);
-        }
-
-    public:
-        char str[MAXLEN];
-        int len;
-
-        int suff[MAXLEN]; // suff[i]: the i-th *sorted* suffix
-        int *rank;         // rank[i]: the rank of the i-th *original* suffix
+        int suff[MAXLEN];	// suff[i]: the i-th *sorted* suffix
+        int *rank, *nrank;	// rank[i]: the rank of the i-th *original* suffix
         // suff[i] = j <=> rank[j] = i
 
         // LCP(i, j): the Longest Common Prefix of the i-th and j-th *sorted* suffixes
-        int hgt[MAXLEN];  //  hgt[i]: LCP(i - 1, i)
+        int hgt[MAXLEN];	// hgt[i]: LCP(i - 1, i)
 
-        void reset()
+        int rbuf[2][MAXLEN]; // used for ranking
+        int cnt[MAXLEN]; // used for counting sort
+
+        void csort(int k, int m)
         {
-            memset(rbuf, 0, sizeof rbuf);
-            memset(cnt, 0, sizeof cnt);
-            memset(suff, 0, sizeof suff);
-            memset(hgt, 0, sizeof hgt);
+            memset(cnt, 0, sizeof(int) * m);
+
+            int t = 0;
+            for (int i = n - k; i < n; i++) nrank[t++] = i;
+            for (int i = 0; i < n; i++)
+                if (suff[i] >= k) nrank[t++] = suff[i] - k;
+
+            for (int i = 0; i < n; i++) cnt[rank[i]]++;
+            for (int i = 1; i < m; i++) cnt[i] += cnt[i - 1];
+            for (int i = n - 1; i >= 0; i--) {
+                t = nrank[i];
+                suff[--cnt[rank[t]]] = t;
+            }
         }
 
         void build()
         {
-            len = strlen(str);
+            n = strlen(s);
             rank = rbuf[0];
             nrank = rbuf[1];
 
-            for (int i = 0; i < len; i++) {
-                rank[i] = str[i];
+            for (int i = 0; i < n; i++) {
+                rank[i] = s[i];
                 suff[i] = i;
             }
-            csort(0, MAXCHAR);
+            int m = MAXCHAR;
+            csort(0, m);
 
-            for (int k = 1, maxrank = MAXCHAR; maxrank != len; k <<= 1) {
-                csort(k, maxrank + 1);
+            for (int k = 1, r = 1; r < n; k <<= 1, m = r + 1) {
+                csort(k, m);
 
-                maxrank = nrank[suff[0]] = 1;
-                for (int i = 1; i < len; i++) {
+                r = nrank[suff[0]] = 1;
+                for (int i = 1; i < n; i++) {
                     if (rank[suff[i - 1]] == rank[suff[i]] && rank[suff[i - 1] + k] == rank[suff[i] + k]) {
-                        nrank[suff[i]] = maxrank;
+                        nrank[suff[i]] = r;
                     } else {
-                        nrank[suff[i]] = ++maxrank;
+                        nrank[suff[i]] = ++r;
                     }
                 }
 
                 swap(rank, nrank);
             }
-            for (int i = 0; i < len; i++) rank[i]--;
+            for (int i = 0; i < n; i++) rank[i]--;
         }
 
         void calc_hgt()
         {
-            for (int i = 0, j, k = 0; i < len; hgt[rank[i++]] = k)
-                for (k ? k-- : 0, j = rank[i] ? suff[rank[i] - 1] : len; str[i + k] == str[j + k]; k++)
-                    ;
+            for (int i = 0, k = 0; i < n; i++) {
+                if (!rank[i]) continue;
+
+                int t = suff[rank[i] - 1];
+                while (s[i + k] == s[t + k]) k++;
+                hgt[rank[i]] = k;
+                if (k) k--;
+            }
         }
 };
 
@@ -106,18 +101,18 @@ char s[100010];
 int main()
 {
     scanf("%s", s);
-    for (int i = 0; s[i] != '\0'; i++) sa.str[sa.len++] = s[i];
-    int mid = sa.len;
-    sa.str[sa.len++] = 1;
+    for (int i = 0; s[i] != '\0'; i++) sa.s[sa.n++] = s[i];
+    int mid = sa.n;
+    sa.s[sa.n++] = 1;
     scanf("%s", s);
-    for (int i = 0; s[i] != '\0'; i++) sa.str[sa.len++] = s[i];
-    sa.str[sa.len] = '\0';
+    for (int i = 0; s[i] != '\0'; i++) sa.s[sa.n++] = s[i];
+    sa.s[sa.n] = '\0';
 
     sa.build();
     sa.calc_hgt();
 
     int ans = 0;
-    for (int i = 1; i < sa.len; i++)
+    for (int i = 1; i < sa.n; i++)
         if (sa.hgt[i] > ans && ((sa.suff[i - 1] < mid && sa.suff[i] > mid) || (sa.suff[i - 1] > mid && sa.suff[i] < mid)))
             ans = sa.hgt[i];
 
--- Templates/suffix-array_lcp.cc
+++ Templates/suffix-array_lcp.cc
@@ -10,9 +10,9 @@
 
 #include <cstdio>
 #include <cstring>
+#include <cmath>
 #include <utility>
 #include <algorithm>
-#include <cmath>
 
 using std::pair;
 using std::make_pair;
@@ -23,111 +23,110 @@ using std::swap;
 class Suffix_Array {
     // use Doubling Algorithm
 
-    private:
+    public:
         const static int MAXLEN = 40010;
         const static int MAXCHAR = 128;
 
-        int *nrank;
-        int rbuf[2][MAXLEN]; // used for ranking
+        int n;			// length of s
+        char s[MAXLEN];
 
-        int cnt[MAXLEN]; // used for counting sort
-
-        void csort(int k, int range)
-        {
-            int t = 0;
-            for (int i = len - k; i < len; i++) nrank[t++] = i;
-            for (int i = 0; i < len; i++)
-                if (suff[i] >= k) nrank[t++] = suff[i] - k;
-
-            for (int i = 0; i < len; i++) cnt[rank[i]]++;
-            for (int i = 1; i < range; i++) cnt[i] += cnt[i - 1];
-            for (int i = len - 1, j; i >= 0; i--) {
-                j = nrank[i];
-                suff[--cnt[rank[j]]] = j;
-            }
-
-            memset(cnt, 0, sizeof(int) * range);
-        }
-
-    public:
-        char str[MAXLEN];
-        int len;
-
-        int suff[MAXLEN]; // suff[i]: the i-th *sorted* suffix
-        int *rank;         // rank[i]: the rank of the i-th *original* suffix
+        int suff[MAXLEN];	// suff[i]: the i-th *sorted* suffix
+        int *rank, *nrank;	// rank[i]: the rank of the i-th *original* suffix
         // suff[i] = j <=> rank[j] = i
 
         // LCP(i, j): the Longest Common Prefix of the i-th and j-th *sorted* suffixes
-        int hgt[MAXLEN];  //  hgt[i]: LCP(i - 1, i)
+        int hgt[MAXLEN];	// hgt[i]: LCP(i - 1, i)
+
+        int rbuf[2][MAXLEN]; // used for ranking
+        int cnt[MAXLEN]; // used for counting sort
 
         int maxsuff[20][MAXLEN], minhgt[20][MAXLEN];
 
-        void reset()
+        void csort(int k, int m)
         {
-            memset(rbuf, 0, sizeof rbuf);
-            memset(cnt, 0, sizeof cnt);
-            memset(suff, 0, sizeof suff);
-            memset(hgt, 0, sizeof hgt);
+            memset(cnt, 0, sizeof(int) * m);
+
+            int t = 0;
+            for (int i = n - k; i < n; i++) nrank[t++] = i;
+            for (int i = 0; i < n; i++)
+                if (suff[i] >= k) nrank[t++] = suff[i] - k;
+
+            for (int i = 0; i < n; i++) cnt[rank[i]]++;
+            for (int i = 1; i < m; i++) cnt[i] += cnt[i - 1];
+            for (int i = n - 1; i >= 0; i--) {
+                t = nrank[i];
+                suff[--cnt[rank[t]]] = t;
+            }
         }
 
         void build()
         {
-            len = strlen(str);
+            n = strlen(s);
             rank = rbuf[0];
             nrank = rbuf[1];
 
-            for (int i = 0; i < len; i++) {
-                rank[i] = str[i];
+            for (int i = 0; i < n; i++) {
+                rank[i] = s[i];
                 suff[i] = i;
             }
-            csort(0, MAXCHAR);
+            int m = MAXCHAR;
+            csort(0, m);
 
-            for (int k = 1, maxrank = MAXCHAR; maxrank != len; k <<= 1) {
-                csort(k, maxrank + 1);
+            for (int k = 1, r = 1; r < n; k <<= 1, m = r + 1) {
+                csort(k, m);
 
-                maxrank = nrank[suff[0]] = 1;
-                for (int i = 1; i < len; i++) {
+                r = nrank[suff[0]] = 1;
+                for (int i = 1; i < n; i++) {
                     if (rank[suff[i - 1]] == rank[suff[i]] && rank[suff[i - 1] + k] == rank[suff[i] + k]) {
-                        nrank[suff[i]] = maxrank;
+                        nrank[suff[i]] = r;
                     } else {
-                        nrank[suff[i]] = ++maxrank;
+                        nrank[suff[i]] = ++r;
                     }
                 }
 
                 swap(rank, nrank);
             }
-            for (int i = 0; i < len; i++) rank[i]--;
+            for (int i = 0; i < n; i++) rank[i]--;
         }
 
         void calc_hgt()
         {
-            for (int i = 0, j, k = 0; i < len; hgt[rank[i++]] = k)
-                for (k ? k-- : 0, j = rank[i] ? suff[rank[i] - 1] : len; str[i + k] == str[j + k]; k++)
-                    ;
+            for (int i = 0, k = 0; i < n; i++) {
+                if (!rank[i]) continue;
+
+                int t = suff[rank[i] - 1];
+                while (s[i + k] == s[t + k]) k++;
+                hgt[rank[i]] = k;
+                if (k) k--;
+            }
         }
 
         void calc_lcp()
         {
-            memcpy(maxsuff[0], suff, sizeof(int) * len);
-            memcpy(minhgt[0], hgt, sizeof(int) * len);
-            for (int i = 1; 1 << i <= len; i++)
-                for (int j = 0; j + (1 << i) <= len; j++) {
+            memcpy(maxsuff[0], suff, sizeof(int) * n);
+            memcpy(minhgt[0], hgt, sizeof(int) * n);
+            for (int i = 1; 1 << i <= n; i++)
+                for (int j = 0; j + (1 << i) <= n; j++) {
                     maxsuff[i][j] = max(maxsuff[i - 1][j], maxsuff[i - 1][j + (1 << (i - 1))]);
                     minhgt[i][j] = min(minhgt[i - 1][j], minhgt[i - 1][j + (1 << (i - 1))]);
                 }
 
         }
 
-        pair<int, int> lcp(int l, int r) // [l, r)
+        pair<int, int> lcp(int l, int r) // lcp of *sorted* suffixes between [l, r)
         {
             int idx = log2(r - l);
             int pos = max(maxsuff[idx][l], maxsuff[idx][r - (1 << idx)]);
 
-            l++;
-            idx = log2(r - l);
-            int res = min(minhgt[idx][l], minhgt[idx][r - (1 << idx)]);
+            int length;
+            if (l + 1 == r) {
+                length = n - suff[l];
+            } else {
+                idx = log2(r - ++l);
+                length = min(minhgt[idx][l], minhgt[idx][r - (1 << idx)]);
+            }
 
-            return make_pair(res, pos);
+            return make_pair(length, pos);
         }
 };
 
@@ -137,33 +136,29 @@ int main()
 {
     int m;
     while (scanf("%d", &m), m) {
-        scanf("%s", sa.str);
-        if (m == 1) {
-            printf("%d %d\n", strlen(sa.str), 0);
-            continue;
-        }
+        memset(&sa, 0, sizeof(Suffix_Array));
+
+        scanf("%s", sa.s);
 
         sa.build();
         sa.calc_hgt();
         sa.calc_lcp();
 
         int longest = 0, pos;
-        for (int i = 0; i + m <= sa.len; i++) {
+        for (int i = 0; i + m <= sa.n; i++) {
             pair<int, int> p = sa.lcp(i, i + m);
             if (p.first > longest) {
                 longest = p.first;
                 pos = p.second;
+            } else if (p.first == longest && p.second > pos) {
+                pos = p.second;
             }
-            else if (p.first == longest && p.second > pos) pos = p.second;
         }
 
         if (longest) printf("%d %d\n", longest, pos);
         else puts("none");
-
-        sa.reset();
     }
 
     return 0;
 }
-
 
